77eeaf680c53f69349f293be7db0d735c2e5b009
马雷斯银 <40496272+MaleiSiyin@users.noreply.github.com>
2020-04-22 12:54:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,6 +10,8 @@ sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_genera
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
+cd package/lean
+git clone https://github.com/Advanced-noob/luci-theme-atmaterial.git
 cd package
 mkdir sakura
 git clone https://github.com/destan19/OpenAppFilter && mv -f OpenAppFilter/* ./
@@ -17,4 +19,3 @@ git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/garypang13/luci-app-eqos
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git
-git clone https://github.com/Advanced-noob/luci-theme-atmaterial.git
